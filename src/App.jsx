edb9a9c09c969ea786251e2c92d09b010a67eab6
shreytalreja25 <shreytalreja25@gmail.com>
2025-01-23 02:07:13 +0530

    fixed mt

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LoanForm from "./components/LoanForm";  // Ensure correct path
 
 function App() {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+    <div className="d-flex justify-content-center align-items-center mt-5 vh-100 bg-light">
       <LoanForm />
     </div>
   );
